bf0c1e1331ab9cf3243dfaf56ce49b535edebd23
KAY S <KS.KWANRUAN@GMAIL.com>
2020-05-25 17:32:43 +1000

    get city and weather information

---
+++ weather-app/scripts/forecast.js
@@ -0,0 +1,31 @@
+const key = "4GOYQuBqV6bmHArZe3oEDrzFRNaDnNHv";
+//get weather information
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+
+// get city information
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+  const response = await fetch(base + query);
+  const data = await response.json();
+  return data[0];
+};
+
+getCity("sydney")
+  .then((data) => {
+    return getWeather(data.Key);
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// ("http://dataservice.accuweather.com/locations/v1/countries/{regionCode}");
